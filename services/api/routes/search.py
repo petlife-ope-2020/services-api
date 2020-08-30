fd5c17996ecce131910d2042879b859b0ccfbedc
erossi-impacta <eric.rossi@aluno.faculdadeimpacta.com.br>
2020-08-30 01:38:12 -0300

    Adds the two routes that will be available on this API
    Signed-off-by: erossi-impacta <eric.rossi@aluno.faculdadeimpacta.com.br>

---
+++ services/api/routes/search.py
@@ -0,0 +1,9 @@
+from flask_restful import Resource
+
+
+class Search(Resource):
+    """ For a client to search for the services a petshop provides."""
+
+    @staticmethod
+    def post():
+        pass
--- services/app.py
+++ services/app.py
@@ -6,12 +6,18 @@ from flask_restful import Api
 from cheroot.wsgi import PathInfoDispatcher
 from cheroot.wsgi import Server as WSGIServer
 
+from services.api.routes.manage import Manage
+from services.api.routes.search import Search
+
 APP = Flask(__name__)
 API = Api(APP)
 PORT = int(os.getenv('PORT', '8080'))
 
 DISPATCHER = PathInfoDispatcher({'/': APP})
 SERVER = WSGIServer(('0.0.0.0', PORT), DISPATCHER)
+
+API.add_resource(Manage, '/manage')
+API.add_resource(Search, '/search')
 
 if __name__ == '__main__':
     SERVER.safe_start()
