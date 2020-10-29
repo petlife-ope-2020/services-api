7530fff94c1ac68b2c31ca86260960103715fbd8
erossi-impacta <eric.rossi@aluno.faculdadeimpacta.com.br>
2020-10-28 23:55:43 -0300

    Adds addition logic
    For a shop to add itself as a provider of a service. When a petshop wants to list a certain service in their list of provided services, they create (if it still doesn't exist) a shared service, that will be updated by subsequent shops.
    
    Signed-off-by: erossi-impacta <eric.rossi@aluno.faculdadeimpacta.com.br>

---
+++ services/api/services/addition.py
@@ -0,0 +1,42 @@
+from flask_restful import abort
+from flask.json import jsonify
+
+from services.utils.db.adapter_factory import get_mongo_adapter
+from services.api.body_parsers.addition import AdditionParser
+
+
+class AdditionService:
+    """ Service for a shop to add itself as a provider of a listed service.
+    """
+    def __init__(self):
+        self.parser = AdditionParser()
+
+    def add(self):
+        """ Gets the fields with the parser and
+            resolves them into an object to be appended on the list
+            of providers for the chosen service.
+
+            Returns
+                updated_service (object): The updated service with the new provider
+        """
+        doc = self.parser.fields
+        self._resolve_fields_to_db_object(doc)
+        updated_service = self._insert_in_mongo(doc)
+        return jsonify(updated_service)
+
+    @staticmethod
+    def _insert_in_mongo(doc):
+        mongo = get_mongo_adapter()
+        try:
+            return mongo.add_self(doc)
+        except KeyError as error:
+            abort(409, extra=f'{error}')
+
+    @staticmethod
+    def _resolve_fields_to_db_object(doc):
+        doc['available_in'] = {
+            'petshop_username': doc['petshop_username'],
+            'petshop_name': doc['petshop_name']
+        }
+        del doc['petshop_username']
+        del doc['petshop_name']
